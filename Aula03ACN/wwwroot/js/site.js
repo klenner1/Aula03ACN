a8fef2462cbedd93895cd7d4a1dd3b1ac731cdc0
klenner <klenne.al@gmail.com>
2018-08-23 22:08:40 -0300

    commit

--- Aula03ACN/wwwroot/js/site.js
+++ Aula03ACN/wwwroot/js/site.js
@@ -1,1 +1,7 @@
-﻿// Write your JavaScript code.
+﻿
+function calc() {
+    var c = $("#c").val();
+    $.get("~/home/Convert/"+ c, function (data, status) {
+        alert("Data: " + data.val + "\nStatus: " + status);
+    });
+}
